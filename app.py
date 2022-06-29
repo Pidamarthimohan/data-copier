668f2a341b1d87c126414fd78791643a7d8fa98b
Pidamarthimohan <Pidamarthi.mohan@clairvoyantsoft.com>
2022-06-29 07:16:57 +0530

    playing with git

--- app.py
+++ app.py
@@ -1,6 +1,33 @@
+# # import pandas as pd
+# def main():
+#     # The file name is hardcoded and assigned to fp.
+#     # fp = 'C:\\Users\\Pidamarthi Mohan\\research\\data\\retail_db_json\\order_items\\part-r-00000-6b83977e-3f20-404b-9b5f-29376ab1419e'
+#
+#
+#     # df = pd.read_json(fp, lines=True)
+# # docker
+#     # df.count()
+#     # print(df.count())
+#     # print(df.describe())
+#     #
+#
+#
+#
+#     # print(df.columns)
+#     # print(df.dtypes)
+#     #
+#     # print(df[['order_item_order_id', 'order_item_subtotal']])
+#     # df['order_item_order_id'] == 2
+#     # print(df[df['order_item_order_id'] == 2])
+# #
+#
+# if __name__ == "__main__":
+#     main()
+
 def main():
-    print("Hello World!")
+    print("Hello World from ITVersity")
 
 
 if __name__ == "__main__":
     main()
+
