56f573865bc63f4017cf96871e4d4de64eee9fc4
Pidamarthimohan <Pidamarthi.mohan@clairvoyantsoft.com>
2022-06-30 08:23:32 +0530

    Read and write logic added to data copier

--- app.py
+++ app.py
@@ -1,31 +1,23 @@
-# # import pandas as pd
-# def main():
-#     # The file name is hardcoded and assigned to fp.
-#     # fp = 'C:\\Users\\Pidamarthi Mohan\\research\\data\\retail_db_json\\order_items\\part-r-00000-6b83977e-3f20-404b-9b5f-29376ab1419e'
-#
-#
-#     # df = pd.read_json(fp, lines=True)
-# # docker
-#     # df.count()
-#     # print(df.count())
-#     # print(df.describe())
-#     #
-#
-#
-#
-#     # print(df.columns)
-#     # print(df.dtypes)
-#     #
-#     # print(df[['order_item_order_id', 'order_item_subtotal']])
-#     # df['order_item_order_id'] == 2
-#     # print(df[df['order_item_order_id'] == 2])
-# #
-#
-# if __name__ == "__main__":
-#     main()
+import os
+import sys
+from read import get_json_reader
+from write import load_db_table
+
+def prcoess_table(BASE_DIR, conn, table_name):
+    json_reader = get_json_reader(BASE_DIR, table_name)
+    for df in json_reader:
+        load_db_table(df, conn, table_name, df.columns[0])
 
 def main():
-    print("Hello World from ITVersity")
+    BASE_DIR = os.environ.get('BASE_DIR')
+    table_names = sys.argv[1].split(',')
+    configs = dict(os.environ.items())
+    conn = f'postgresql://{configs["DB_USER"]}:{configs["DB_PASS"]}@{configs["DB_HOST"]}:{configs["DB_PORT"]}/{configs["DB_NAME"]}'
+    for table_name in table_names:
+        prcoess_table(BASE_DIR, conn, table_name)
+
+
+
 
 
 if __name__ == "__main__":
